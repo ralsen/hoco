4277c3af831269412469f4aec6172145d67b4a84
Ralph Follrichs <ralph.follrichs@hs-hannover.de>
2024-07-26 11:22:05 +0200

    all drivers get devhandler-instance and the mBlock

--- progs/SNSW-001X16EU.py
+++ progs/SNSW-001X16EU.py
@@ -5,14 +5,17 @@ import threading
 logger = logging.getLogger(__name__)
 
 class driver:
-    def __init__(self, iBlock):
+    def __init__(self, driver, iBlock, mBlock):
         self.iBlock = iBlock
+        self.mBlock = mBlock
+        self.drv = driver
         threading.Thread(target=self._check, daemon=True).start()    
         pass
     
     def _check(self):
-        i= 0
+        i = 0
         while True:
-            logger.debug(f"{self.iBlock['hostname']}: {i}")
+            on = self.drv.isDeviceOnline(self.mBlock['ip'])
+            logger.debug(f"{self.iBlock['hostname']}: {i} is {on}")
             i+=1
             time.sleep(1)
