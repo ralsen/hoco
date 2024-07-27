2a7d79ba1a7d78c6d4b1b68f39c841f8ea37ab98
ralsen <follrichs@gmx.de>
2024-07-27 17:44:09 +0200

    Testfunction added

--- progs/DS1820.py
+++ progs/DS1820.py
@@ -11,7 +11,8 @@ class driver:
         self.drv = driver
         threading.Thread(target=self._check, daemon=True).start()    
         logger.info(f"driver {self.iBlock['modul']} installed for {self.iBlock['name']}")        
-    
+        self.test()
+        
     def _check(self):
         i = 0
         while True:
@@ -19,3 +20,7 @@ class driver:
             logger.debug(f"{self.iBlock['hostname']}: {i} is {on}")
             i+=1
             time.sleep(self.iBlock['time'])
+    
+    def test(self):
+        print("###########################")
+        logger.debug(f"!!! bin in Test() !!! {self}")
