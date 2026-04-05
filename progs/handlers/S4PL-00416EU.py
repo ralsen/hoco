e66c614ebe9a874e576964b26acab998d63fac0e
ralsen <follrichs@gmx.de>
2026-04-05 15:27:46 +0200

    hanlder for shelly power strip4

--- progs/handlers/S4PL-00416EU.py
+++ progs/handlers/S4PL-00416EU.py
@@ -8,5 +8,12 @@ logging.getLogger('urllib3').setLevel(logging.WARNING)
 @Dispatcher.register("S4PL-00416EU")
 def handle_S4PL(self):
     logger.debug("Handling: S4PL-00416EU")
-    data = json.loads(self.this['response'].text)
+    data = {}
+    data['name'] = self.this['device']['Hostname']
+    data['Type'] = self.this['device']['Type']
+    data['IP'] = self.this['device']['IP']
+    power = json.loads(self.this['response'].text)
+    apowers = [v["apower"] for v in power.values() if isinstance(v, dict) and "apower" in v]
+    for i in range(len(apowers)):
+        data['power_'+ str(i)] = apowers[i]
     return data
